fe336a258cb832c8707d2fef745088d0d6ea034e
Leslie Alexander Sinchiguano Saltos <69779736+lsinchiguanos@users.noreply.github.com>
2021-08-29 08:19:20 -0500

    A

--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
 const express = require('express'); //llamamos a Express
 const app = express();
-
+const port = (process.env.PORT || 4000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(require('./routes/rutas'));
 
-app.set('port', process.env.PORT || 3000);
-
 // iniciamos nuestro servidor
-app.listen();
-console.log('API escuchando en el puerto');
+app.listen(port);
+console.log('API escuchando en el puerto ' + port);
